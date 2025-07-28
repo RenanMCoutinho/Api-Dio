9a6cf18656a37759fbe7ceaa677f16caab1e8dd1
Renan Marcilio Coutinho <renan.coutinho.11@gmail.com>
2025-07-28 14:41:03 -0300

    Add files via upload

---
+++ API/Controllers/UsuarioController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+
+
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet("obter-data-hora")]
+        public IActionResult ObterDataHora()
+        {
+            var obj = new
+
+            {
+                Data = DateTime.Now.ToLongDateString(),
+                Hora = DateTime.Now.ToLongTimeString()
+            };
+            return Ok(obj);
+        }
+        [HttpGet("Apresentar/{nome}")]
+        public IActionResult Apresentar(string nome)
+        {
+            var mensagem = $"Olá, {nome}! Bem-vindo(a) à nossa API.";
+            return Ok(new { mensagem });
+        }
+
+    }
+
+
+}
